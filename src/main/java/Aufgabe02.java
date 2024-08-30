bc523a720afece7265c72f91425ed81fcaf31f06
JoshuaSchreiber <joshua.schreiber.bg@bs-gn.de>
2024-08-30 15:30:44 +0200

    Task2

--- src/main/java/Aufgabe02.java
+++
@@ -1,8 +0,0 @@
-public class Aufgabe02 {
-    public static int returnResult(int vatlue, int maske, String operation){
-
-        return 0;
-    }
-
-
-}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(a2returnResult(0b10010011, 0b10011100, "&"));
+        System.out.println(a2returnResult(0b10010011, 0b10011100, "|"));
+        System.out.println(a2returnResult(0b10010011, 0b10011100, "^"));
     }
+
+    public static String a2returnResult(int value, int maske, String operation){
+        int result = 0;
+        if(operation == "&"){
+            result = value & maske;
+        }else if(operation == "|"){
+            result = value | maske;
+        }else if(operation == "^"){
+            result = value ^ maske;
+        }
+        return String.format("%8s", Integer.toBinaryString(result)).replace(' ', '0');
+    }
+
 }
